e62c24bec3945bf4d71d03688bfe880d39cbc8ac
Fely <40307073+jfelya@users.noreply.github.com>
2020-02-09 20:35:17 -0400

    jhkhil

---
+++ procesos/procesos.js
@@ -0,0 +1,7 @@
+function calculo1() {
+	document.getElementById("texto1").innerHTML = "<br>Holi 1<br><br>";
+}
+
+function calculo2() {
+	document.getElementById("texto2").innerHTML = "<br>Holi 2<br><br>";
+}
